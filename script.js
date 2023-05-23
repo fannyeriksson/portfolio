1d5d3c87216bc8e62e22a5cf995d581509183f9c
fannyeriksson <123304940+fannyeriksson@users.noreply.github.com>
2023-05-23 13:35:42 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+//För att headern inte ska försvinna när man skrollar ner//
+const header = document.querySelector("header");
+window.addEventListener ("scroll", function() {
+	header.classList.toggle ("sticky", window.scrollY > 100);
+});
+
+//Hamburgermenyn//
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+	menu.classList.toggle('bx-x');
+	navlist.classList.toggle('open');
+};
+
+window.onscroll = () => {
+	menu.classList.remove('bx-x');
+	navlist.classList.remove('open');
+};
